8350fd73e2525c487317cd8f174439052469e66f
Francisco Miguel DIaz Montes <fmdiazmontes87@gmail.com>
2020-02-19 14:59:26 +0100

    particles v1

--- FranFrontDev/src/app/app.module.ts
+++ FranFrontDev/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FfContactComponent } from './ff-contact/ff-contact.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @NgModule({
@@ -25,8 +26,8 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
 
-    MatButtonModule
-
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FranFrontDev/src/app/ff-intro/ff-intro.component.ts
+++ FranFrontDev/src/app/ff-intro/ff-intro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var particlesJS: any;
+
 @Component({
   selector: 'app-ff-intro',
   templateUrl: './ff-intro.component.html',
@@ -12,6 +14,7 @@ export class FfIntroComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    particlesJS.load('particles-js', '../assets/json/particles.json', null);
   }
 
   goToContact(): void {
